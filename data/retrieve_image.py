6457db0a536c64ac833ff954f3d18f1ff5f6a918
jhu20 <jh3916@columbia.edu>
2022-04-29 22:07:05 -0400

    add data download

--- data/retrieve_image.py
+++ data/retrieve_image.py
@@ -5,4 +5,4 @@ with open('nocaps_test_image_info.json') as json_data:
     d = json.load(json_data)
     images = d["images"]
     for obj in images:
-        urllib.request.urlretrieve(obj['coco_url'], "nocaps/" + str(obj["id"]).zfill(6)+".jpg")
+        urllib.request.urlretrieve(obj['coco_url'], "/home/ubuntu/jeff/dataset/nocaps/" + str(obj["id"]).zfill(6)+".jpg")
